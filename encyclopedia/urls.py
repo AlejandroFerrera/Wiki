7c1bd9f6a851454dd9b979dd62a3af5aaf811f77
Alejandro_Ferrera <aferrerah1421@gmail.com>
2022-09-14 02:34:47 -0300

    added entry pages and updated index page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("not-found/", views.not_found, name="not_found"),
+    path("wiki/<str:title>", views.page, name="page")
+
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-
+from http.client import HTTPResponse
+from django.shortcuts import render, redirect
+from markdown2 import Markdown
 from . import util
+
+
 
 
 def index(request):
@@ -8,3 +11,17 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def page(request, title):
+
+    markdowner = Markdown()
+    entry = util.get_entry(title)
+
+    if entry:
+        entry = markdowner.convert(entry)
+        return render(request, 'encyclopedia/page.html', {'title': title, 'entry': entry })  
+    
+    return redirect('not_found')
+
+def not_found(request):
+    return render(request, 'encyclopedia/not-found.html')
